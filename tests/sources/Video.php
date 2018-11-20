4e6c8614ac403c55eb97dc34a527934659532977
Johnny Saymon <saymon@ciebit.com.br>
2018-11-20 15:15:06 -0300

    Criado testes das classes File e Youtube

---
+++ tests/sources/Video.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ciebit\VideosTests;
+
+use Ciebit\Videos\Status;
+use PHPUnit\Framework\TestCase;
+
+abstract class Video extends TestCase
+{
+    protected const DATE_PUBLICATION = '2018-11-20 14:17:22';
+    protected const DESCRIPTION = 'Description Teste';
+    protected const ID = '2';
+    protected const SOURCE_ID = '33';
+    protected const STATUS = STATUS::ACTIVE;
+    protected const TITLE = 'Title Test';
+    protected const URI = 'uri-test';
+}
---
+++ tests/sources/YoutubeTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Ciebit\VideosTests;
+
+use DateTime;
+use Ciebit\Videos\Youtube;
+use Ciebit\Videos\Status;
+use Ciebit\VideosTests\Video as VideosTests;
+use PHPUnit\Framework\TestCase;
+
+class YoutubeTest extends VideosTests
+{
+    public function testCreateFromManual(): void
+    {
+        $videoYoutube = new Youtube(
+            self::TITLE,
+            self::URI,
+            new Status(self::STATUS)
+        );
+        $videoYoutube
+        ->setDatePublication(new DateTime(self::DATE_PUBLICATION))
+        ->setDescription(self::DESCRIPTION)
+        ->setId(self::ID)
+        ->setSourceId(self::SOURCE_ID)
+        ;
+
+        $this->assertEquals(self::DATE_PUBLICATION, $videoYoutube->getDatePublication()->format('Y-m-d H:i:s'));
+        $this->assertEquals(self::DESCRIPTION, $videoYoutube->getDescription());
+        $this->assertEquals(self::ID, $videoYoutube->getId());
+        $this->assertEquals(self::SOURCE_ID, $videoYoutube->getSourceId());
+        $this->assertEquals(self::STATUS, $videoYoutube->getStatus()->getValue());
+        $this->assertEquals(self::TITLE, $videoYoutube->getTitle());
+        $this->assertEquals(self::URI, $videoYoutube->getUri());
+    }
+}
