78974a83284a46982eab8d453bde6a77c82a426a
Johnny Saymon <saymon@ciebit.com.br>
2018-11-21 16:23:00 -0300

    Corrigido migration para aceitar nulo em algumas colunas

--- database/migrations/20181120201655_v1.php
+++ database/migrations/20181120201655_v1.php
@@ -7,13 +7,14 @@ class V1 extends AbstractMigration
 {
     public function change()
     {
-        ($this->table('videos', ['comment' => 'version:1']))
-        ->addColumn('sourceId', 'string')
+        ($this->table('cb_videos', ['comment' => 'version:1']))
+        ->addColumn('source_id', 'string', ['null' => true])
         ->addColumn('title', 'string')
-        ->addColumn('description', 'string')
-        ->addColumn('date_publication', 'datetime')
+        ->addColumn('description', 'string', ['null' => true])
+        ->addColumn('date_publication', 'datetime', ['null' => true])
+        ->addColumn('type', 'string', ['limit' => 50])
         ->addColumn('uri', 'string')
-        ->addColumn('status', 'integer', ['limit' => 1])
+        ->addColumn('status', 'integer', ['limit' => 1, 'signed' => false])
         ->create()
         ;
     }
